8a52683576ee4f7a04c6de9554c0c350337ad5f0
mrdevsam <mdshoykotislam05@gmail.com>
2024-04-08 01:48:07 +0600

    first commit

---
+++ src/main/java/com/example/springdatajpa_order_service/SpringdatajpaOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatajpa_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatajpaOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdatajpaOrderServiceApplication.class, args);
+	}
+
+}
