d9abf40fed3e5e368a8bc0a10e089406f349fea9
mrdevsam <mdshoykotislam05@gmail.com>
2024-04-14 21:48:05 +0600

    cascading delete operation

--- src/test/java/com/example/springdatajpa_order_service/repos/OrderHeaderRepoTest.java
+++ src/test/java/com/example/springdatajpa_order_service/repos/OrderHeaderRepoTest.java
@@ -2,12 +2,14 @@ package com.example.springdatajpa_order_service.repos;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.orm.jpa.JpaObjectRetrievalFailureException;
 
 import com.example.springdatajpa_order_service.domain.Customer;
 import com.example.springdatajpa_order_service.domain.OrderApproval;
@@ -97,6 +99,30 @@ public class OrderHeaderRepoTest {
         assertNotNull(fetchedOrder);
         assertNotNull(fetchedOrder.getId());
         // because of using cascade type 'persist', we don't need to invoke OrderLine repository to save a new OrderLine object related to OrderHeader object
+
+    }
+
+    @Test
+    void testDeleteCascade() {
+        var orderHeader = new OrderHeader();
+        var customer = new Customer();
+        var orderLine = new OrderLine();
+
+        customer.setCustomerName("New new customer");
+        orderHeader.setCustomer(curepo.save(customer));
+        orderLine.setQuantityOrdered(3);
+        orderLine.setProduct(product);
+        orderHeader.addOrderLine(orderLine);
+
+        var savedOrder = repo.saveAndFlush(orderHeader);
+
+        System.out.println("Order saved and flushed");
+
+        repo.deleteById(savedOrder.getId());
+
+        assertThrows(JpaObjectRetrievalFailureException.class, () -> {
+            var fetchedOrder = repo.getReferenceById(savedOrder.getId());
+        });
 
     }
 }
