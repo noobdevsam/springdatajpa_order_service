3eefd9401e591a16b3c973ba5b82f6909eab36ec
mrdevsam <mdshoykotislam05@gmail.com>
2024-08-12 23:24:22 +0600

    adding bootstrap class

---
+++ src/main/java/com/example/springdatajpa_order_service/bootstrap/Bootstrap.java
@@ -0,0 +1,14 @@
+package com.example.springdatajpa_order_service.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bootstrap implements CommandLineRunner{
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("---------------------------------");
+    }
+    
+}
