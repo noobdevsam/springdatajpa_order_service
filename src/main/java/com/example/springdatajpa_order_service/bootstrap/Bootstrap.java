0e984a964cc5801a20564f4adb16a44298e5fff5
mrdevsam <mdshoykotislam05@gmail.com>
2024-08-13 01:30:57 +0600

    using proxy mode

--- src/main/java/com/example/springdatajpa_order_service/bootstrap/Bootstrap.java
+++ src/main/java/com/example/springdatajpa_order_service/bootstrap/Bootstrap.java
@@ -3,37 +3,27 @@ package com.example.springdatajpa_order_service.bootstrap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.example.springdatajpa_order_service.repos.OrderHeaderRepo;
+import com.example.springdatajpa_order_service.services.BootstrapOrderHeaderService;
 
 @Component
 public class Bootstrap implements CommandLineRunner{
 
     @Autowired
-    OrderHeaderRepo oHeaderRepo;
+    BootstrapOrderHeaderService service;
 
-    @Transactional
     @Override
     public void run(String... args) throws Exception {
         System.out.println("---------------------------------");
-        System.out.println("Demonstrating lazy initialization error due to out of hibernate context");
 
-        var orderHeader = oHeaderRepo.findById(1L).get();
-        orderHeader.getOrderLines().forEach(ol -> {
-            System.out.println(ol.getProduct().getDescription());
+        // this is an external method call using proxy mode
+        service.readOrderData();
 
-            // this call happens out of transactional context for OrderHeaderRepo
-            ol.getProduct().getCategories().forEach(cat -> {
-                System.out.println(cat.getDescription());
-            });
-        });
+        /*
+         * Transactional Proxy Mode
+         *      Here, run() is a external method but readOrderData() is
+         *      a internal method. We need a proxy to call internal method
+         *      inside the external method.
+         */
     }
-
-    /*
-     * If we comment out the @Transactional annotation, we will get
-     *      org.hibernate.LazyInitializationException: failed to lazily initialize a collection of
-     *           role: com.example.springdatajpa_order_service.domain.Product.categories: could not initialize proxy - no Session
-     */
     
 }
