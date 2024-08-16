6ca2a0f8a55ff6f8292340b911f8ae800829649b
mrdevsam <mdshoykotislam05@gmail.com>
2024-08-16 14:28:50 +0600

    modified findById in product repo

--- src/main/java/com/example/springdatajpa_order_service/repos/ProductRepo.java
+++ src/main/java/com/example/springdatajpa_order_service/repos/ProductRepo.java
@@ -3,6 +3,8 @@ package com.example.springdatajpa_order_service.repos;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.lang.NonNull;
+
 import com.example.springdatajpa_order_service.domain.Product;
 import jakarta.persistence.LockModeType;
 
@@ -10,8 +12,9 @@ public interface ProductRepo extends JpaRepository<Product, Long>{
 
     Optional<Product> findByDescription(String description);
 
+    @NonNull
     @Override
     @Lock(value = LockModeType.PESSIMISTIC_WRITE)
-    Optional<Product> findById(Long id);
+    Optional<Product> findById(@NonNull Long id);
 
 }
